971478c2c25ca0233b2bf738f606ea2f333cd062
Samugodoy1 <samuelgodoylima@gmail.com>
2025-05-15 20:42:12 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ def create_app():
     app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     
     # Configure database
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get["DATABASE_URL"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
         "pool_recycle": 300,
         "pool_pre_ping": True,
